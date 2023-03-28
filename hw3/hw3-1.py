7b4a51e4a89ca803c2a5ffc5178a78c1bbfc6ab2
Jihye Park <p931991@gmail.com>
2023-03-29 00:37:13 +0900

    good

--- hw3/hw3-1.py
+++ hw3/hw3-1.py
@@ -7,7 +7,6 @@ print(msg.decode())    #수신 후 문자열처리
 #본인의 이름을 문자열로 전송
 a = "Jihye Park"
 sock.send(a.encode())
-
 #본인의 학번을 수신 후 출력
 a = sock.recv(1024)       #정수로 받음
 print(int.from_bytes(a, 'little'))     #엔디언 변환 출력
